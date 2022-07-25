84df09e63a3c497ae64df69f02f336d566755bc3
Md. Asaduzzaman <asad@mpower-social.com>
2022-07-25 17:03:48 +0600

    Improved View and Fixed Sort Issue

--- lib/views/submitted_form_screen.dart
+++ lib/views/submitted_form_screen.dart
@@ -14,8 +14,8 @@ import 'package:m_survey/widgets/no_data_found_msg.dart';
 import 'package:m_survey/widgets/progress_bar.dart';
 
 class SubmittedFormScreen extends StatelessWidget {
-  late final Function? wp;
-  late final Function? hp;
+  Function? wp;
+  Function? hp;
   final ProjectListFromLocalDb? project;
   SubmittedFormController controller = Get.find();
 
@@ -33,7 +33,6 @@ class SubmittedFormScreen extends StatelessWidget {
       child: Scaffold(
         appBar: baseAppBar(title: 'App Submission Response'),
         body: Container(
-          padding: const EdgeInsets.all(10),
           height: hp!(100),
           width: wp!(100),
           child: Column(
@@ -63,17 +62,20 @@ class SubmittedFormScreen extends StatelessWidget {
   }
 
   Widget _filter() {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-      children: [
-        Flexible(
-          flex: 7,
-          child: Row(
-            children: [
-              Flexible(
-                flex: 2,
-                child: Obx(
+    return Padding(
+      padding: EdgeInsets.only(left: 20, right: 12),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Flexible(
+            flex: 7,
+            child: Row(
+              children: [
+                Obx(
                   () => Checkbox(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(3),
+                    ),
                     materialTapTargetSize: MaterialTapTargetSize.shrinkWrap,
                     value: controller.isCheckedAll.value,
                     onChanged: (v) {
@@ -83,56 +85,59 @@ class SubmittedFormScreen extends StatelessWidget {
                     },
                   ),
                 ),
-              ),
-              Flexible(
-                flex: 5,
-                child: SizedBox(
-                  height: 40,
-                  child: Obx(
-                    () => DropdownButtonFormField<ProjectListFromLocalDb>(
-                        isExpanded: true,
-                        value: controller.selectedProject,
-                        items: controller.projectList
-                            .map(
-                                (e) => DropdownMenuItem<ProjectListFromLocalDb>(
-                                      value: e,
-                                      child: Text(e.projectName!),
-                                    ))
-                            .toList(),
-                        decoration: CommonStyle.textFieldStyle(
-                          verPadding: 8,
-                          horPadding: 10,
-                        ),
-                        onChanged: (v) {
-                          controller.filter(v?.id ?? 0);
-                        }),
+                SizedBox(width: 10),
+                Flexible(
+                  flex: 5,
+                  child: SizedBox(
+                    height: 40,
+                    child: Obx(
+                      () => DropdownButtonFormField<ProjectListFromLocalDb>(
+                          isExpanded: true,
+                          value: controller.selectedProject,
+                          items: controller.projectList
+                              .map((e) =>
+                                  DropdownMenuItem<ProjectListFromLocalDb>(
+                                    value: e,
+                                    child: Text(e.projectName!),
+                                  ))
+                              .toList(),
+                          decoration: CommonStyle.textFieldStyle(
+                            verPadding: 8,
+                            horPadding: 10,
+                          ),
+                          onChanged: (v) {
+                            controller.filter(v?.id ?? 0);
+                          }),
+                    ),
                   ),
                 ),
-              ),
-            ],
-          ),
-        ),
-        Flexible(
-          flex: 3,
-          child: InkWell(
-            onTap: () {
-              controller.ascOrDesc.value = !controller.ascOrDesc.value;
-              controller.sortByDate();
-            },
-            child: Container(
-              constraints: const BoxConstraints(minHeight: 30),
-              padding: const EdgeInsets.all(2),
-              decoration: BoxDecoration(
-                  border: Border.all(color: grey),
-                  borderRadius: BorderRadius.circular(10)),
-              child: const Icon(
-                AppIcons.group_15,
-                size: 35,
-              ),
+              ],
             ),
           ),
-        )
-      ],
+          Flexible(
+            flex: 3,
+            child: InkWell(
+              customBorder: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(10)),
+              onTap: () {
+                controller.ascOrDesc.value = !controller.ascOrDesc.value;
+                controller.sortByDate();
+              },
+              child: Container(
+                constraints: const BoxConstraints(minHeight: 30),
+                padding: const EdgeInsets.all(2),
+                decoration: BoxDecoration(
+                    border: Border.all(color: grey),
+                    borderRadius: BorderRadius.circular(10)),
+                child: const Icon(
+                  AppIcons.group_15,
+                  size: 35,
+                ),
+              ),
+            ),
+          )
+        ],
+      ),
     );
   }
 
@@ -143,10 +148,11 @@ class SubmittedFormScreen extends StatelessWidget {
           : controller.isCheckList.length == 0
               ? noDataFound()
               : ListView.separated(
+                  padding: EdgeInsets.only(left: 10, right: 10),
                   itemCount: controller.isCheckList.length,
                   itemBuilder: (ctx, i) {
                     SubmittedFormListData data =
-                        controller.submittedFormList[i]!;
+                        controller.isCheckList[i].submittedFormListData!;
                     return Container(
                       width: wp!(100),
                       padding: EdgeInsets.only(left: 10, right: 10),
@@ -161,6 +167,9 @@ class SubmittedFormScreen extends StatelessWidget {
                             children: [
                               Obx(
                                 () => Checkbox(
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(3),
+                                    ),
                                     materialTapTargetSize:
                                         MaterialTapTargetSize.shrinkWrap,
                                     value: (controller
@@ -171,6 +180,7 @@ class SubmittedFormScreen extends StatelessWidget {
                                               controller.submittedFormList[i]);
                                     }),
                               ),
+                              SizedBox(width: 10),
                               Column(
                                 crossAxisAlignment: CrossAxisAlignment.start,
                                 children: [
