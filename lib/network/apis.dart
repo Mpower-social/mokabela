0a4ffff7430970d6cac6542e4661548ab98659ec
hatanvir <tanvirhasan553@gmail.com>
2023-10-19 15:53:36 +0600

    Qa url added

--- lib/network/apis.dart
+++ lib/network/apis.dart
@@ -3,7 +3,7 @@ class Apis{
 
 
   //static var url = 'http://192.168.19.19:3008';//dev
-  //static var qa = 'https://msurvey-api.qa.mpower-social.com';//qa
+  static var qa = 'https://msurvey-api.qa.mpower-social.com';//qa
  // static var dev = 'https://msurvey-api.dev.mpower-social.com';//dev
   static var local = 'https://msurvey-api.demo.mpower-social.com';//dev
   static var live = 'https://msurvey-api.mpower-social.com';//dev
@@ -13,14 +13,14 @@ class Apis{
   //static var odkRelatedUrl = 'http://192.168.19.16:9037';//dev
   //static var odkRelatedUrl = 'http://192.168.19.16:8092/';//dev
   //static var odkRelatedUrl = 'http://192.168.19.16:9037';//dev
-  //static var odkRelatedUrlQa = 'https://msurvey-product.qa.mpower-social.com';//qa
+  static var odkRelatedUrlQa = 'https://msurvey-product.qa.mpower-social.com';//qa
   //static var odkRelatedUrlDev = 'https://msurvey-product.dev.mpower-social.com';//dev
   static var odkRelatedUrlDemo = 'https://msurvey-product.demo.mpower-social.com';//demo
   static var odkRelatedUrlLive = 'https://msurvey-product.mpower-social.com';//demo
   //static var odkRelatedUrl = 'http://192.168.22.59:8111';//local
 
 
-  static var baseUrl = '$live/msurvey/api';//dev
+  static var baseUrl = '$qa/msurvey/api';//dev
 
   static var login = '$baseUrl/auth/login';
   static var refreshToken = '$baseUrl/auth/refresh-token';
@@ -28,7 +28,7 @@ class Apis{
   static getUserByUserName(userName) => '$baseUrl/user/get-user-by-username?username=$userName';
   static var getFormList = '$baseUrl/project/forms/get-form-list-all';
   static var getAllFormList = '$baseUrl/project/forms/get-all-form-configuration';
-  static var getSubmittedFormList = '$odkRelatedUrlLive/msurvey/forms/instance/list/';
+  static var getSubmittedFormList = '$odkRelatedUrlQa/msurvey/forms/instance/list/';
   static var getRevertedFormList = '$baseUrl/project/forms/get-all-reverted-instances';
-  static var submitForm = '$odkRelatedUrlLive/msurvey/forms/instance/submit/';
+  static var submitForm = '$odkRelatedUrlQa/msurvey/forms/instance/submit/';
 }
